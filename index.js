47daeb162ee1f4d24ac1a589a3da33fef0c3b377
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-12 21:59:34 +0100

    add operate function that chooses calculator function

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ function multiply (n1, n2) {
 function divide (n1, n2) {
     return n1/n2;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            addition(num1, num2);
+            break;
+        case "-":
+            subtraction(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            alert("error");
+    } 
+}
