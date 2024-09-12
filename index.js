7ddd6940ae7f16fb3e5d003e23b91b6bb23045d3
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-12 21:52:08 +0100

    add calculator functions, add, subtract, multiply, divide

---
+++ index.js
@@ -0,0 +1,16 @@
+// Calculation functions
+function addition(n1, n2) {
+    return n1 + n2;
+}
+
+function subtraction (n1, n2) {
+    return n1-n1;
+};
+
+function multiply (n1, n2) {
+    return n1*n2;
+}
+
+function divide (n1, n2) {
+    return n1/n2;
+}
